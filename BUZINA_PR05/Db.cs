d49411aa57fa1620343fa2b667c6f02e5dc07032
Buzina <mariabuzinaa@yandex.ru>
2024-10-02 21:11:10 +0300

    feat: add new module Db.cs

---
+++ BUZINA_PR05/Db.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUZINA_PR05
+{
+    static class Db
+    {
+    }
+}
